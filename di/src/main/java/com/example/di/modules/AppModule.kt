ef9d5951dc534a2d26776b9512384f45ee04a9eb
RomanEysmond <eysmondroman@gmail.com>
2026-03-20 17:29:57 +0300

    AppModule

--- di/src/main/java/com/example/di/modules/AppModule.kt
+++ di/src/main/java/com/example/di/modules/AppModule.kt
@@ -1,5 +1,18 @@
 package com.example.di.modules
 
+import android.content.Context
+import android.content.SharedPreferences
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Singleton
+
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
